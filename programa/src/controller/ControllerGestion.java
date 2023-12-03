425ea876809ba07ae15eefa2a7c97d147dadd644
Matias Gluck <matiasugluck@gmail.com>
2023-12-03 17:36:44 -0300

    Facturas por dia proveedor

--- programa/src/controller/ControllerGestion.java
+++ programa/src/controller/ControllerGestion.java
@@ -1,7 +1,9 @@
 package controller;
 
+import dao.FacturaDAO;
 import dao.OrdenDeCompraDAO;
 import dao.ProveedorDAO;
+import model.Factura;
 import model.LineaOrden;
 import model.OrdenDeCompra;
 import dto.CuentaCorrienteDTO;
@@ -9,12 +11,14 @@ import model.Proveedor;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 public class ControllerGestion {
 
     private static ControllerGestion INSTANCE = null;
 
+    private static FacturaDAO facturaDAO;
     private static OrdenDeCompraDAO ordenDeCompraDAO;
     private static ProveedorDAO proveedorDAO;
 
@@ -24,18 +28,29 @@ public class ControllerGestion {
         ControllerGestion.ordenDeCompraDAO = ordenDeCompraDAO;
     }
 
-    public void facturasPorDiaProveedor(int idProvedor){
-
-    };
-    private ControllerGestion(ProveedorDAO proveedorDAO, OrdenDeCompraDAO ordenDeCompraDAO){
+    private ControllerGestion(FacturaDAO facturaDAO, ProveedorDAO proveedorDAO, OrdenDeCompraDAO ordenDeCompraDAO) {
+        this.facturaDAO = facturaDAO;
         this.proveedorDAO = proveedorDAO;
         this.ordenDeCompraDAO = ordenDeCompraDAO;
     }
+
+    public double facturasPorDiaProveedor(int idProvedor, Date fecha){
+        List<Factura> facturas = this.facturaDAO.getAll(Factura.class);
+        double montoTotal = 0;
+        for (Factura factura: facturas) {
+            if (factura.getCuit() == idProvedor || factura.getFecha() == fecha) {
+                montoTotal += factura.getMonto();
+            }
+        }
+        return montoTotal;
+    };
+    
     public static synchronized ControllerGestion getInstances() throws Exception {
         if(INSTANCE == null) {
+            FacturaDAO FacturaDAO = new FacturaDAO(Factura.class, getPathOutModel(Factura.class.getSimpleName()));
             ProveedorDAO ProveedorDAO = new ProveedorDAO(Proveedor.class, getPathOutModel(Proveedor.class.getSimpleName()));
             OrdenDeCompraDAO OrdenDeCompraDAO = new OrdenDeCompraDAO(OrdenDeCompra.class, getPathOutModel(OrdenDeCompra.class.getSimpleName()));
-            INSTANCE = new ControllerGestion(ProveedorDAO, OrdenDeCompraDAO);
+            INSTANCE = new ControllerGestion(FacturaDAO, ProveedorDAO, OrdenDeCompraDAO);
         }
         return INSTANCE;
     }
