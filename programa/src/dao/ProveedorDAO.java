d8dac266d682fb25e5b64424f71bfcd02dcd599a
Matias Gluck <matiasugluck@gmail.com>
2023-12-04 23:20:43 -0300

    feat: ejemplo de dao en memoria

--- programa/src/dao/ProveedorDAO.java
+++ programa/src/dao/ProveedorDAO.java
@@ -1,11 +1,50 @@
 package dao;
 
+import enums.TipoResponsabilidad;
 import model.Proveedor;
 import util.GenericDAO;
 
-public class ProveedorDAO extends GenericDAO<Proveedor> {
+import java.util.Date;
 
-    public ProveedorDAO(Class<Proveedor> clase, String file) throws Exception {
-        super(clase, file);
+public class ProveedorDAO {
+
+    public ProveedorDAO() throws Exception {
+        Proveedor textilVendoTelas = new Proveedor(
+                1,
+                TipoResponsabilidad.RESPONSABLE_INSCRIPTO,
+                "Textil Vendo Telas",
+                "Textil Vendo Telas",
+                "Una calle muy linda 123",
+                null,
+                null,
+                20,
+                new Date(),
+                null);
+
+
+    Proveedor despegarPuntoCom = new Proveedor(
+            2,
+            TipoResponsabilidad.RESPONSABLE_INSCRIPTO,
+            "Despegar Punto Com",
+            "Despegar Punto Com",
+            "Una calle muy linda 456",
+            null,
+            null,
+            20,
+            new Date(),
+            null);
+
+
+    Proveedor pescaderiaDeTito = new Proveedor(
+            3,
+            TipoResponsabilidad.MONOTRIBUTO,
+            "La Pescaderia de Tito",
+            "Tito Baratito",
+            "El Muelle 1024",
+            null,
+            null,
+            5,
+            new Date(),
+            null);
     }
 }
